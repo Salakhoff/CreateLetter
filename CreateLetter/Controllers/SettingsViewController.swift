96178e6f1f0dd0a7ac42942db57e8d1b8e03f897
Ilfat <salakhoff.ilfat@gmail.com>
2023-10-02 13:13:03 +0300

    first settings

---
+++ CreateLetter/Controllers/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  CreateLetter
+//
+//  Created by Ильфат Салахов on 02.10.2023.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
