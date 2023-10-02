d8435fb0c78c7c9d7545fca4c9861343a025d8a4
Ilfat <salakhoff.ilfat@gmail.com>
2023-10-02 19:20:39 +0300

    Настройка интерфейса  LetterVC

--- CreateLetter/Controllers/LetterViewController.swift
+++ CreateLetter/Controllers/LetterViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 final class LetterViewController: UIViewController {
+    
+    private lazy var letterTextView = LetterTextView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setConstraints()
+    }
+}
+
+// MARK: SetupView
+private extension LetterViewController {
+    func setupView() {
         view.backgroundColor = .systemBackground
+        
+        navigationItem.title = "Письмо"
+        
+        view.addSubview(letterTextView)
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            letterTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            letterTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            letterTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            letterTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+        ])
     }
 }
 
